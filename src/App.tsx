e8c43ced5a66a0a90c37044d848aaa9a896dc611
damian <the.dmian@live.cl>
2023-04-19 18:03:21 -0400

    ADD completed tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Todos } from './components/Todos'
+import { type Todo as TodoType, type TodoId } from './types'
 
 const mockTodos = [
   {
@@ -22,13 +23,26 @@ const mockTodos = [
 const App = (): JSX.Element => {
   const [todos, setTodos] = useState(mockTodos)
 
-  const handleRemove = (id: string): void => {
+  const handleRemove = ({ id }: TodoId): void => {
     const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+  const handleCompleted = ({ id, completed }: Pick<TodoType, 'id' | 'completed'>): void => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+      return todo
+    })
     setTodos(newTodos)
   }
   return (
     <div className='todoapp'>
       <Todos
+      onToggleCompleteTodo={handleCompleted}
       todos={todos}
       onRemoveTodo={handleRemove}
       />
